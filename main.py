80cd94268fc9d5ab4f58ce2d8b34e8300296ae07
Ryan Truman <ryan.truman@hotmail.com>
2025-08-03 20:24:20 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=messages,
+)
+
+if "--verbose" in sys.argv:
+    print(f"{response.text}\nUser prompt: {sys.argv[1]}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(response.text)
